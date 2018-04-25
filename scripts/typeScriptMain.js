4756b6752593035311d781e508e2dd4b96dc5b6f
casey gruse <caseygruse1@gmail.com>
2018-04-25 15:17:24 -0700

    images with values fail

--- scripts/typeScriptMain.js
+++ scripts/typeScriptMain.js
@@ -1,7 +1,7 @@
 window.onload = function () {
-    startPlaying();
-    firstRoll();
     var rollNum = 1;
+    firstRoll();
+    startPlaying();
 };
 function startPlaying() {
     document.getElementById("letsPlay").addEventListener("click", function () {
@@ -12,15 +12,35 @@ function startPlaying() {
     });
 }
 function firstRoll() {
-    var images = createImageArray();
+    var imagesValue = createImageValueArray();
     document.getElementById("roll").onclick = function () {
         for (var i = 0; i < 5; i++) {
-            var randImage = images[Math.floor(Math.random() * 6)];
-            document.getElementsByTagName("img")[i].setAttribute("src", randImage);
+            var randValue = imagesValue[Math.floor(Math.random() * 6)];
+            document.getElementsByTagName("img")[i].setAttribute("value", randValue);
         }
+        asignImagesToImageTags();
     };
+}
+function retrieveValueFromImgTagsInArray() {
+    var imageValueArray = [5];
+    for (var i = 0; i < 5; i++) {
+        imageValueArray[i] = parseInt(document.getElementsByTagName("img")[i].getAttribute("value"));
+    }
+    return imageValueArray;
+}
+function asignImagesToImageTags() {
+    var imageValue = retrieveValueFromImgTagsInArray();
+    var images = createImageArray();
+    for (var i = 0; i < 5; i++) {
+        document.getElementsByTagName("img")[i].setAttribute("src", images[imageValue[i - 1]]);
+    }
 }
 function createImageArray() {
     var images = ["dice/1.png", "dice/2.png", "dice/3.png", "dice/4.png", "dice/5.png", "dice/6.png"];
+    var imageValue = [1, 2, 3, 4, 5, 6];
     return images;
+}
+function createImageValueArray() {
+    var imageValue = ["1", "2", "3", "4", "5", "6"];
+    return imageValue;
 }
--- typeScriptMain.ts
+++ typeScriptMain.ts
@@ -1,8 +1,13 @@
 window.onload = function(){
-    startPlaying();
+    let rollNum:number = 1;
+    
     firstRoll();
     
-    let rollNum:number = 1;
+    startPlaying();
+    
+    
+    
+    
 };
 
 /**
@@ -14,31 +19,82 @@ function startPlaying():void{
     let player2:string = (<HTMLInputElement>document.getElementById("player2Name")).value;
     (<HTMLSpanElement>document.getElementById("player1")).textContent = player1;
     (<HTMLSpanElement>document.getElementById("player2")).textContent = player2;
+    
     })
 }   
 
+
+
+
+/**
+ * hooks a click event to roll which will roll all 5 dice.
+ */
 function firstRoll():void{
-    let images = createImageArray();
+    //let images = createImageArray();
+    let imagesValue = createImageValueArray();
     document.getElementById("roll").onclick = function(){
         for(var i = 0; i < 5; i++){
-            let randImage:string =images[Math.floor(Math.random() * 6)]
+            let randValue:string = imagesValue[Math.floor(Math.random() * 6)]
+            //let randImage:string =images[Math.floor(Math.random() * 6)]
             //let randTag:string = "slot"+i;
-            document.getElementsByTagName("img")[i].setAttribute("src", randImage);
+            //document.getElementsByTagName("img")[i].setAttribute("src", randImage);
+            document.getElementsByTagName("img")[i].setAttribute("value", randValue);
         }
+        
+        asignImagesToImageTags();
+    }  
+}
+/**
+ * retrieves value from img tags so the correct dice number can be placed
+ * in the right positions/
+ * the 5 in the loop is the number of imgTags
+ */
+function retrieveValueFromImgTagsInArray():number[]{
+    
+    let imageValueArray = [5];
+    for(var i = 0; i < 5; i++){
+        imageValueArray[i] = parseInt(document.getElementsByTagName("img")[i].getAttribute("value"));
+    }
+    return imageValueArray;
+}
+/**
+ * puts the dice pictures into the image tags where their corresponding numbers are.
+ */
+function asignImagesToImageTags(){
+    let imageValue = retrieveValueFromImgTagsInArray();
+    let images = createImageArray();
+    for(var i = 0; i < 5; i++){
+        document.getElementsByTagName("img")[i].setAttribute("src", images[imageValue[i-1]]);
     }
 }
 
 
 
 
-
-
+//creates the image array.
 function createImageArray():string[]{
     let images = ["dice/1.png", "dice/2.png", "dice/3.png", "dice/4.png", "dice/5.png", "dice/6.png"];
-    return images;
-        
+    let imageValue = [1, 2, 3, 4, 5, 6];
+    return images;    
+}
+
+function createImageValueArray():string[]{
+    let imageValue = ["1", "2", "3", "4", "5", "6"];
+    return imageValue;    
 }
 
     
 
 
+
+// function images(src:string, value:string):void{
+//     let pictureSlots = document.getElementById("imgList")
+//     var myImage = new Image(100, 100);
+//     myImage.src = src;
+//     myImage.nodeValue = value;
+//     pictureSlots.appendChild(myImage);
+// }
+
+// function createImageTagArray(){
+//     images("1.png", "1");
+// }
